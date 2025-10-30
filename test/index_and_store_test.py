971c5ab276df4735822c1fe21ac2b07bd6b4c084
wade-ary <161520052+wade-ary@users.noreply.github.com>
2025-10-30 12:59:02 -0400

    test with new material: Japans population decline

--- test/index_and_store_test.py
+++ test/index_and_store_test.py
@@ -25,14 +25,19 @@ encryption = AESEncryption()
 data_store = LocalStorageManager()
 
 ingestion_pipeline = BasicIngestionPipeline(loader,preprocess, embedder, data_store, encryption)
-file_path1 = os.path.join(SAMPLE_DIR, "europol.pdf")
-file_path2 = os.path.join(SAMPLE_DIR, "global_initiatives.pdf")
-file_path3 = os.path.join(SAMPLE_DIR, "un_office_drugs.pdf")
+file_path1 = os.path.join(SAMPLE_DIR, "fullarticle_2024-0010.pdf")
+file_path2 = os.path.join(SAMPLE_DIR, "96648955-en.pdf")
+file_path3 = os.path.join(SAMPLE_DIR, "commentary351e.pdf")
+file_path4 = os.path.join(SAMPLE_DIR, "Japans-Forgotten-Countryside-Demographic-Crisis-and-Revival-Strategies.pdf")
+file_path5 = os.path.join(SAMPLE_DIR, "ko250824a1.pdf")
+
 
 
 ingestion_pipeline.ingest(file_path1, "public")
 ingestion_pipeline.ingest(file_path2,"employee")
 ingestion_pipeline.ingest(file_path3, "admin")
+ingestion_pipeline.ingest(file_path4, "admin")
+ingestion_pipeline.ingest(file_path5, "public")
 
 
 
